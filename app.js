0655b47d22e74dbf5162a01b4213f238e51c83b1
carry <guxuelong@360.net>
2018-11-13 15:07:45 +0800

    test lowdb-1

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require('express')
 var app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ posts: [], user: {}, count: 0 })
+  .write()
+
 app.get('/users/:userId/books/:bookId', function (req, res) {
-  res.send(req.params)
+	db.set('user', req.params)
+  .write()
+  res.send('ok');
 })
+
+app.get('/users', function (req, res) {
+  res.send(db.get('user').value());
+})
+
+
 
 app.get('/flights/:from-:to', function (req, res) {
   res.send(req.params)
